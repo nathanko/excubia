d5620fe59fb986c08ea49f080b4c295c2738e7bc
Nathan Ko <nathanchko@gmail.com>
2017-09-16 14:33:03 -0400

    Changed port to 8080

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ def video_feed():
     return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
