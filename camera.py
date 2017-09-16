69912d05ac0f9786a802a7ba99bd79d48f172f49
Renee Leung <renee.leung@edu.uwaterloo.ca>
2017-09-16 16:15:46 -0400

    Added eye detection

--- camera.py
+++ camera.py
@@ -20,9 +20,10 @@ class VideoCamera(object):
         
         # https://realpython.com/blog/python/face-detection-in-python-using-a-webcam/
 
-        cascPath = "haarcascade_frontalface_default.xml"
+        cascPath = "haarcascades/haarcascade_frontalface_default.xml"
+        cascEyePath = "haarcascades/haarcascade_eye.xml"
         faceCascade = cv2.CascadeClassifier(cascPath)
-
+        eyeCascade = cv2.CascadeClassifier(cascEyePath)
 
         #Cool AI Stuff
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -35,10 +36,16 @@ class VideoCamera(object):
             #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
             flags=cv2.CASCADE_SCALE_IMAGE
         )
-
+        
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = image[y:y+h, x:x+w]
+
+            eyes = eyeCascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex, ey),(ex+ew, ey+eh),(255, 0, 255), 2)
         print ("Number of faces: {}".format(len(faces)))
 
 
