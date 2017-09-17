6b80e0cb1e77a74d6b3a16e7f9fe693862013ed4
Nathan Ko <nathanchko@gmail.com>
2017-09-17 11:49:45 -0400

    Working demo

--- camera.py
+++ camera.py
@@ -6,20 +6,20 @@ import os
 from imutils import face_utils
 import numpy as np
 import imutils
-import dlib
-import pymongo
-from pymongo import MongoClient
+# import dlib
+# import pymongo
+# from pymongo import MongoClient
 
 # http://www.pyimagesearch.com/2017/04/17/real-time-facial-landmark-detection-opencv-python-dlib/
 #https://medium.com/@ageitgey/machine-learning-is-fun-part-4-modern-face-recognition-with-deep-learning-c3cffc121d78
 
 args = {'cascPath': 'data/haarcascade_frontalface_default.xml',
         'shape_predictor': 'data/shape_predictor_68_face_landmarks.dat'}
-client = MongoClient('mongodb://excubia:Incredible54-@ds139884.mlab.com:39884/excubia-faces')
-db = client['excubia-faces']
-print(db)
-collection = db.alpha
-print(collection)
+# client = MongoClient('mongodb://excubia:Incredible54-@ds139884.mlab.com:39884/excubia-faces')
+# db = client['excubia-faces']
+# print(db)
+# collection = db.alpha
+# print(collection)
 
 
 
@@ -34,8 +34,9 @@ class VideoCamera(object):
         # initialize dlib's face detector (HOG-based) and then create
         # the facial landmark predictor
         #print("[INFO] loading facial landmark predictor...")
-        detector = dlib.get_frontal_face_detector()
-        predictor = dlib.shape_predictor(args["shape_predictor"])
+
+        # detector = dlib.get_frontal_face_detector()
+        # predictor = dlib.shape_predictor(args["shape_predictor"])
 
         success, image = self.video.read()
         
@@ -46,21 +47,21 @@ class VideoCamera(object):
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        # detect faces in the grayscale frame
-        rects = detector(gray, 0)
-        faceLandmarks = []
+        # # detect faces in the grayscale frame
+        # rects = detector(gray, 0)
+        # faceLandmarks = []
 
-        # loop over the face detections
-        for rect in rects:
-            # determine the facial landmarks for the face region, then convert
-            # facial landmark coordinates to a Numpy array
-            shape = predictor(gray, rect)
-            shape = face_utils.shape_to_np(shape)
-            # loop over the (x, y)-coordinates for the facial landmark and draw
-            for (x, y) in shape:
-                cv2.circle(image, (x, y), 2, (0, 0, 255), -1)
-                #print("({},{})".format(x,y))
-                faceLandmarks.append((x,y))
+        # # loop over the face detections
+        # for rect in rects:
+        #     # determine the facial landmarks for the face region, then convert
+        #     # facial landmark coordinates to a Numpy array
+        #     shape = predictor(gray, rect)
+        #     shape = face_utils.shape_to_np(shape)
+        #     # loop over the (x, y)-coordinates for the facial landmark and draw
+        #     for (x, y) in shape:
+        #         cv2.circle(image, (x, y), 2, (0, 0, 255), -1)
+        #         #print("({},{})".format(x,y))
+        #         faceLandmarks.append((x,y))
         
         faces = faceCascade.detectMultiScale(
             gray,
@@ -74,15 +75,15 @@ class VideoCamera(object):
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            post = {"landmarks": []}
-            for (s,t) in faceLandmarks:
-                if ( x <= s <= x+s and y <= t <= y+h):
-                    # Add to database
-                    post['landmarks'].append([str(s),str(t)])
-            try:
-                post_id = db.posts.insert_one(post).inserted_id
-            except Exception as e:
-                pass
+            # post = {"landmarks": []}
+            # for (s,t) in faceLandmarks:
+            #     if ( x <= s <= x+s and y <= t <= y+h):
+            #         # Add to database
+            #         post['landmarks'].append([str(s),str(t)])
+            # try:
+            #     post_id = db.posts.insert_one(post).inserted_id
+            # except Exception as e:
+            #     pass
             # Eyes detection
             #roi_gray = gray[y:y+h, x:x+w]
             #roi_color = image[y:y+h, x:x+w]
